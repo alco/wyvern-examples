4575201c0e26f25546038b18f6a63671b25c143b
Alexei Sholik <alcosholik@gmail.com>
2014-05-04 03:39:28 +0300

    Populate example with templates

--- apps/prerender/lib/prerender.ex
+++ apps/prerender/lib/prerender.ex
@@ -1,3 +1,20 @@
 defmodule Prerender do
+  @templates_dir "lib/prerender/templates"
+  @output_dir "output"
 
+  def render_views() do
+    File.mkdir_p!(@output_dir)
+
+    views = [
+      ["layouts/base", "index"],
+      ["layouts/base", "layouts/navbar", "red"],
+      ["layouts/base", "layouts/navbar", "green"],
+      ["layouts/base", "layouts/navbar", "blue"],
+    ]
+    Enum.each(views, fn layers ->
+      result = Wyvern.render_view(layers, templates_dir: @templates_dir)
+      path = Path.join(@output_dir, List.last(layers) <> ".html")
+      File.write!(path, result)
+    end)
+  end
 end
